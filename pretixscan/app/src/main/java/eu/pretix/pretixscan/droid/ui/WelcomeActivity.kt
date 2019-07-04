4fd47435f7f43e57aa784f9a8e77d7ae4bd9365a
Raphael Michel <mail@raphaelmichel.de>
2019-07-04 17:08:55 +0200

    Automatically disable camera on Zebra TC2x

--- pretixscan/app/src/main/java/eu/pretix/pretixscan/droid/ui/WelcomeActivity.kt
+++ pretixscan/app/src/main/java/eu/pretix/pretixscan/droid/ui/WelcomeActivity.kt
@@ -1,9 +1,11 @@
 package eu.pretix.pretixscan.droid.ui
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import eu.pretix.pretixscan.droid.AppConfig
 import eu.pretix.pretixscan.droid.R
 import eu.pretix.pretixscan.droid.databinding.ActivityWelcomeBinding
 import kotlinx.android.synthetic.main.activity_welcome.*
@@ -17,6 +19,11 @@ class WelcomeActivity : AppCompatActivity() {
         button2.setOnClickListener {
             val intent = Intent(this, SetupActivity::class.java)
             startActivity(intent)
+        }
+
+        if (Build.BRAND == "Zebra" && (Build.MODEL == "TC20" || Build.MODEL == "TC25")) {
+            val conf = AppConfig(this)
+            conf.useCamera = false
         }
     }
 }
