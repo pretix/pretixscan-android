46fcfdb3843b4d40d5ae0e1da5768c5f0d2f4d95
Raphael Michel <michel@rami.io>
2024-06-10 14:39:07 +0200

    Update logo file

--- img/sizes.sh
+++ img/sizes.sh
@@ -1,27 +1,8 @@
-mkdir -p ../pretixdroid/pretixdroid/app/res/drawable-ldpi/
-mkdir -p ../pretixdroid/app/src/main/res/drawable-mdpi/
-mkdir -p ../pretixdroid/app/src/main/res/drawable-hdpi/
-mkdir -p ../pretixdroid/app/src/main/res/drawable-xhdpi/
-mkdir -p ../pretixdroid/app/src/main/res/drawable-xxhdpi/
-mkdir -p ../pretixdroid/app/src/main/res/drawable-xxxhdpi/
-
-for f in ic_*.svg; do
-	echo $f;
-	inkscape -zf $f -w 36 -e ../pretixdroid/app/res/drawable-ldpi/$(echo $f | sed s/svg/png/);
-	inkscape -zf $f -w 48 -e ../pretixdroid/app/src/main/res/drawable-mdpi/$(echo $f | sed s/svg/png/);
-	inkscape -zf $f -w 72 -e ../pretixdroid/app/src/main/res/drawable-hdpi/$(echo $f | sed s/svg/png/);
-	inkscape -zf $f -w 96 -e ../pretixdroid/app/src/main/res/drawable-xhdpi/$(echo $f | sed s/svg/png/);
-	inkscape -zf $f -w 144 -e ../pretixdroid/app/src/main/res/drawable-xxhdpi/$(echo $f | sed s/svg/png/);
-	inkscape -zf $f -w 192 -e ../pretixdroid/app/src/main/res/drawable-xxxhdpi/$(echo $f | sed s/svg/png/);
+for f in ic_logo.svg; do
+	echo $f-export-
+	inkscape -z $f -w 48 --export-type=PNG --export-filename=../pretixscan/app/src/main/res/drawable-mdpi/$(echo $f | sed s/svg/png/);
+	inkscape -z $f -w 72 --export-type=PNG --export-filename=../pretixscan/app/src/main/res/drawable-hdpi/$(echo $f | sed s/svg/png/);
+	inkscape -z $f -w 96 --export-type=PNG --export-filename=../pretixscan/app/src/main/res/drawable-xhdpi/$(echo $f | sed s/svg/png/);
+	inkscape -z $f -w 144 --export-type=PNG --export-filename=../pretixscan/app/src/main/res/drawable-xxhdpi/$(echo $f | sed s/svg/png/);
+	inkscape -z $f -w 192 --export-type=PNG --export-filename=../pretixscan/app/src/main/res/drawable-xxxhdpi/$(echo $f | sed s/svg/png/);
 done;
-
-for f in logo*.svg; do
-	echo $f;
-	inkscape -zf $f -w 225 -e ../pretixdroid/app/src/main/res/drawable-ldpi/$(echo $f | sed s/svg/png/);
-	inkscape -zf $f -w 300 -e ../pretixdroid/app/src/main/res/drawable-mdpi/$(echo $f | sed s/svg/png/);
-	inkscape -zf $f -w 450 -e ../pretixdroid/app/src/main/res/drawable-hdpi/$(echo $f | sed s/svg/png/);
-	inkscape -zf $f -w 600 -e ../pretixdroid/app/src/main/res/drawable-xhdpi/$(echo $f | sed s/svg/png/);
-	inkscape -zf $f -w 900 -e ../pretixdroid/app/src/main/res/drawable-xxhdpi/$(echo $f | sed s/svg/png/);
-	inkscape -zf $f -w 1200 -e ../pretixdroid/app/src/main/res/drawable-xxxhdpi/$(echo $f | sed s/svg/png/);
-done;
-
