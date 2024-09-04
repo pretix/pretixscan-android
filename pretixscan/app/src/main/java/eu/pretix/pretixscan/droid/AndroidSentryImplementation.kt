e0e49599db5d0da4f3cbd5f6f82005ca7c7cc5aa
Maximilian Richt <richt@rami.io>
2024-09-04 10:31:51 +0200

    Update sentry to 7.14.0

--- pretixscan/app/src/main/java/eu/pretix/pretixscan/droid/AndroidSentryImplementation.kt
+++ pretixscan/app/src/main/java/eu/pretix/pretixscan/droid/AndroidSentryImplementation.kt
@@ -2,16 +2,24 @@ package eu.pretix.pretixscan.droid
 
 
 import eu.pretix.libpretixsync.SentryInterface
+import io.sentry.Breadcrumb
 import io.sentry.Sentry
 
 
 class AndroidSentryImplementation : SentryInterface {
     override fun addHttpBreadcrumb(url: String, method: String, statusCode: Int) {
-        Sentry.addBreadcrumb("$method $url [$statusCode]")
+        val breadcrumb = Breadcrumb().apply {
+            category = "http"
+            message = "$method $url [$statusCode]"
+        }
+        Sentry.addBreadcrumb(breadcrumb)
     }
 
     override fun addBreadcrumb(a: String, b: String) {
-        Sentry.addBreadcrumb("$a $b")
+        val breadcrumb = Breadcrumb().apply {
+            message = "$a $b"
+        }
+        Sentry.addBreadcrumb(breadcrumb)
     }
 
     override fun captureException(t: Throwable) {
@@ -19,7 +27,7 @@ class AndroidSentryImplementation : SentryInterface {
     }
 
     override fun captureException(t: Throwable, message: String) {
-        Sentry.addBreadcrumb(message)
+        Sentry.captureMessage(message)
         Sentry.captureException(t)
     }
 }
