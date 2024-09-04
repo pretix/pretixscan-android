0bf9f83411c2aa64c2ac62f0377ad43497eab15a
Maximilian Richt <richt@rami.io>
2024-09-04 10:26:00 +0200

    Use PendingIntentCompat

--- pretixscan/app/src/main/java/eu/pretix/pretixscan/droid/ui/Utils.kt
+++ pretixscan/app/src/main/java/eu/pretix/pretixscan/droid/ui/Utils.kt
@@ -5,7 +5,7 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Build
+import androidx.core.app.PendingIntentCompat
 import eu.pretix.libpretixsync.Models
 import eu.pretix.pretixscan.droid.AppConfig
 import kotlin.system.exitProcess
@@ -19,11 +19,7 @@ fun wipeApp(ctx: Context) {
 
     val mStartActivity = Intent(ctx, WelcomeActivity::class.java)
     val mPendingIntentId = 123456
-    val mPendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-        PendingIntent.getActivity(ctx, mPendingIntentId, mStartActivity, PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_MUTABLE)
-    } else {
-        PendingIntent.getActivity(ctx, mPendingIntentId, mStartActivity, PendingIntent.FLAG_CANCEL_CURRENT)
-    }
+    val mPendingIntent = PendingIntentCompat.getActivity(ctx, mPendingIntentId, mStartActivity, PendingIntent.FLAG_CANCEL_CURRENT, true)!!
     val mgr = ctx.getSystemService(Context.ALARM_SERVICE) as AlarmManager
     mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent)
     exitProcess(0)
